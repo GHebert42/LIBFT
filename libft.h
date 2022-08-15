03a0ecb8ee7b69cc448d1025ff23455e17956427
GHebert42 <88173405+GHebert42@users.noreply.github.com>
2022-08-15 11:24:31 -0400

    Update libft.h

--- libft.h
+++ libft.h
@@ -50,6 +50,7 @@ size_t	ft_strlcat(char *dest, const char *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t n);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+char	*ft_strcpy(char *s1, char s2);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
