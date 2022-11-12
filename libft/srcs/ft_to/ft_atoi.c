29fc20f80e114dca2f9c576a375ee8e10dfec31b
GHebert42 <88173405+GHebert42@users.noreply.github.com>
2022-11-12 13:04:35 -0500

    Update ft_atoi.c

--- libft/srcs/ft_to/ft_atoi.c
+++ libft/srcs/ft_to/ft_atoi.c
@@ -35,3 +35,53 @@ int	ft_atoi(const char *str)
 		num = num * 10 + (*(str + i++) - '0');
 	return (num * sign);
 }
+
+long long	ps_atoll(char *s)
+{
+	int					i;
+	int					neg;
+	unsigned long long	ret;
+
+	i = 0;
+	ret = 0;
+	neg = 1;
+	while (s[i] == '\t' || s[i] == '\n' || s[i] == '\r' || s[i] == '\v' || \
+		s[i] == '\f' || s[i] == ' ')
+		i++;
+	if (s[i] == '-')
+		neg = -1;
+	if (s[i] == '-' || s[i] == '+')
+		i++;
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		ret = 10 * ret + s[i] - '0';
+		i++;
+	}
+	ret = ret * neg;
+	return (ret);
+}
+
+int	ps_atoi(char *s)
+{
+	int					i;
+	int					neg;
+	unsigned long long	ret;
+
+	i = 0;
+	ret = 0;
+	neg = 1;
+	while (s[i] == '\t' || s[i] == '\n' || s[i] == '\r' || s[i] == '\v' || \
+		s[i] == '\f' || s[i] == ' ')
+		i++;
+	if (s[i] == '-')
+		neg = -1;
+	if (s[i] == '-' || s[i] == '+')
+		i++;
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		ret = 10 * ret + s[i] - '0';
+		i++;
+	}
+	ret = ret * neg;
+	return ((int)ret);
+}
