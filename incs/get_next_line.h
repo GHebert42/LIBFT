b5d28ad108ec0b2ae53847e6051c3abfd325a899
GHebert42 <hebertgermain.qc@gmail.com>
2023-02-23 16:00:55 -0500

    gnl_h

---
+++ incs/get_next_line.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gehebert <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/08 07:49:36 by gehebert          #+#    #+#             */
+/*   Updated: 2021/11/02 08:28:17 by gehebert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+char	*ft_read(int fd);
+char	*ft_get_input(char *buf, int fd);
+char	*ft_set_line(char *buf, char *line);
+char	*get_next_line(int fd);
+
+char    *gnl_substr(char const *s, unsigned int start, size_t len);
+size_t	gnl_strlen(char const *str);
+size_t	gnl_strlcpy(char *dest, const char *src, size_t n);
+size_t	gnl_strlcat(char *dst, const char *src, size_t size);
+int     gnl_strchr_i(const char *s, int c);
+
+#endif
